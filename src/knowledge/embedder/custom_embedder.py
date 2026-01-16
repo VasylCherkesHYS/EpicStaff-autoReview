678875c5ddf1b923b7f702b9eeb193e5a000cc53
ArturDrohomeretskyiHYS <artur.drohomeretskyi@hys-enterprise.com>
2026-01-16 14:04:18 +0200

    hotfix: custom embedder

---
+++ src/knowledge/embedder/custom_embedder.py
@@ -0,0 +1,35 @@
+from typing import List
+import requests
+import os
+
+from .base_embedder import BaseEmbedder
+
+
+class CustomEmbedder(BaseEmbedder):
+
+    def __init__(
+        self, api_key: str = None, model_name: str = None, base_url: str = None
+    ):
+        """Initialize the embedder."""
+        self.base_url = base_url or os.getenv(
+            "CUSTOM_EMBED_BASE_URL"
+        )
+        self.api_key = api_key or os.getenv("CUSTOM_EMBED_API_KEY")
+        self.model_name = model_name or "nomic-embed-text-v2-moe"
+        self.endpoint = f"{self.base_url}"
+
+    def embed(self, text: str) -> List[float]:
+        """Get embedding for text."""
+        text = text.replace("\n", " ")
+
+        response = requests.post(
+            self.endpoint,
+            headers={
+                "Authorization": f"Bearer {self.api_key}",
+                "Content-Type": "application/json",
+            },
+            json={"model": self.model_name, "input": [text]},
+        )
+        response.raise_for_status()
+        return response.json()["embeddings"][0]
+
