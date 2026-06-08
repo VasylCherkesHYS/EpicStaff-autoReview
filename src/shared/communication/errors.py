96d83cb7b6d15021bc76566326d558994f812bf7
BogdanTimchenkoHYS <bogdan-timchenko@hys-enterprise.com>
2026-06-08 12:57:01 +0300

    feat(EST-3003-BE): create base errors for storage and broker

--- src/shared/communication/errors.py
+++ src/shared/communication/errors.py
@@ -1,2 +1,10 @@
 class CommunicationError(Exception):
     """Base error for the communication package."""
+
+
+class StorageError(CommunicationError):
+    """Base error for the storage."""
+
+
+class BrokerError(CommunicationError):
+    """Base error for the broker."""
