47354d7c64532c1f6e00fdbc8474a2d8536a6ed6
BogdanTimchenkoHYS <bogdan-timchenko@hys-enterprise.com>
2026-06-08 13:03:06 +0300

    feat(EST-3003-BE): implement redis storage

--- src/shared/communication/storages/__init__.py
+++ src/shared/communication/storages/__init__.py
@@ -1,7 +1,9 @@
 """Storage implementations and the AbstractStorage contract."""
 
 from .abstract import AbstractStorage
+from .redis_ import RedisStorage
 
 __all__ = [
     "AbstractStorage",
+    "RedisStorage",
 ]
